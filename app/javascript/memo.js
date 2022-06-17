85f35522d96eb66f3f865b7df80d324fdd8ae1d8
TakumaYmamoto <104744174+TakumaYmamoto@users.noreply.github.com>
2022-06-17 22:00:27 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -2,8 +2,8 @@ function post (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    const form = document.getElementById("form")
-    const formDate = new FormData(form);
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
